d76c061789cf6faf1ef508bd264849e982a1c2e4
reeesadh <rheasadhwani@outlook.com>
2026-06-14 22:09:02 -0700

    Completed lab

--- src/main/java/frc/robot/commands/LEDs/BlinkColor.java
+++ src/main/java/frc/robot/commands/LEDs/BlinkColor.java
@@ -7,11 +7,14 @@
  
 package frc.robot.commands.LEDs;
 
+import static frc.robot.parameters.Colors.BLACK;
+
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.parameters.Colors;
 import frc.robot.subsystems.LEDSubsystem;
 
+/** A command to blink the status LEDs a specified color. */
 public final class BlinkColor extends Command {
   // Using 0.2 results in the LEDs to end up as no color
   private static final double BLINK_TIME = 0.19;
@@ -23,20 +26,29 @@ public final class BlinkColor extends Command {
 
   /** Creates a new BlinkColor. */
   public BlinkColor(LEDSubsystem led, Colors color) {
-    // your code here
+    setName(String.format("BlinkColor(%s)", color.name()));
+
+    this.led = led;
+    this.color = color;
+
+    addRequirements(led);
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    // your code here
+    led.fillAndCommitColor(color);
+    isOn = true;
+    blinkTimer.reset();
+    blinkTimer.start();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+
     if (blinkTimer.advanceIfElapsed(BLINK_TIME)) {
-      // your code here
+      led.fillAndCommitColor(isOn ? color : BLACK);
       isOn = !isOn;
     }
   }
--- src/main/java/frc/robot/parameters/Colors.java
+++ src/main/java/frc/robot/parameters/Colors.java
@@ -3,5 +3,31 @@ package frc.robot.parameters;
 import edu.wpi.first.wpilibj.util.Color8Bit;
 
 public enum Colors {
+    BLACK(0,0,0),
+    RED(255, 0, 0),
+    WHITE(255, 255, 255),
+    YELLOW(200, 200, 0);
 
+    private final Color8Bit color;
+    
+    Colors(int red, int green, int blue) {
+        color = new Color8Bit(red, green, blue);
+    }
+
+    public int getRed() {
+        return color.red;
+    }
+    
+    public int getGreen() {
+        return color.green;
+    }
+    
+    public int getBlue() {
+        return color.blue;
+    }
+    
+    public Color8Bit getColor() {
+        return color;
+    }
 }
+
